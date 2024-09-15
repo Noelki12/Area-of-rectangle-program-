f21016d238df6ae6bd5b3fa2cb2bb10448f43798
Noelki12 <noelkipkoech@zetech.ac.ke>
2024-09-15 16:32:21 +0300

    Add files via upload

---
+++ area of rectangle program .cpp
@@ -0,0 +1,12 @@
+//program to find the area of a rectangle 
+#include<stdio.h> 
+int main(){ 
+int width, length,area;
+area=length*width;
+printf("length:\n");
+scanf("%d,& length\n");
+printf("width:\n");
+scanf("%d,&width\n");
+printf("%d, area\n");
+return 0;
+}
